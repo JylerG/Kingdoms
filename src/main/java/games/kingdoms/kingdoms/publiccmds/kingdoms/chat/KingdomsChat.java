00f68b02a88b6d7e5f7a05d3d55288419a8f2fb9
YBR_Bleach <90173411+JylerG@users.noreply.github.com>
2024-07-29 21:19:10 -0500

    Add files via upload

--- src/main/java/games/kingdoms/kingdoms/publiccmds/kingdoms/chat/KingdomsChat.java
+++ src/main/java/games/kingdoms/kingdoms/publiccmds/kingdoms/chat/KingdomsChat.java
@@ -2,7 +2,6 @@ package games.kingdoms.kingdoms.publiccmds.kingdoms.chat;
 
 import games.kingdoms.kingdoms.Kingdoms;
 import games.kingdoms.kingdoms.MessageManager;
-import games.kingdoms.kingdoms.admin.ranks.Rank;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -32,50 +31,20 @@ public class KingdomsChat implements CommandExecutor {
 
             for (Player p : Bukkit.getOnlinePlayers()) {
                 if (plugin.getKingdoms().containsKey(p.getUniqueId().toString())) {
+                    if (plugin.getKingdoms().get(p.getUniqueId().toString()).equals(plugin.getKingdoms().get(player.getUniqueId().toString()))) {
 
-                    String message = String.join(" ", args); // Concatenate args into a single string
+                        String message = String.join(" ", args); // Concatenate args into a single string
 
-                    String rank = plugin.getPlayerRank().get(player.getUniqueId().toString());
-
-                    //Player has Default Rank
-                    if (rank.equals(ChatColor.DARK_GRAY.toString() + ChatColor.BOLD + Rank.DEFAULT)) {
-                        String format = ChatColor.AQUA.toString() + ChatColor.BOLD + "[K] " + ChatColor.WHITE + player.getDisplayName() + ChatColor.AQUA + ": " + ChatColor.LIGHT_PURPLE + message;
-                        p.sendMessage(format);
-                    }
-                    //Player has VIP Rank
-                    if (rank.equals(ChatColor.GREEN.toString() + ChatColor.BOLD + Rank.VIP)) {
-                        String format = ChatColor.AQUA.toString() + ChatColor.BOLD + "[K] " + ChatColor.GREEN + player.getDisplayName() + ChatColor.AQUA + ": " + ChatColor.LIGHT_PURPLE + message;
-                        p.sendMessage(format);
-                    }
-                    //Player has Hero Rank
-                    if (rank.equals(ChatColor.AQUA.toString() + ChatColor.BOLD + Rank.HERO)) {
-                        String format = ChatColor.AQUA.toString() + ChatColor.BOLD + "[K] " + ChatColor.AQUA + player.getDisplayName() + ChatColor.AQUA + ": " + ChatColor.LIGHT_PURPLE + message;
-                        p.sendMessage(format);
-                    }
-                    //Player has JrMod Rank
-                    if (rank.equals(ChatColor.DARK_AQUA.toString() + ChatColor.BOLD + Rank.JRMOD)) {
-                        String format = ChatColor.AQUA.toString() + ChatColor.BOLD + "[K] " + ChatColor.DARK_AQUA + player.getDisplayName() + ChatColor.AQUA + ": " + ChatColor.LIGHT_PURPLE + message;
-                        p.sendMessage(format);
-                    }
-                    //Player has Mod Rank
-                    if (rank.equals(ChatColor.YELLOW.toString() + ChatColor.BOLD + Rank.MOD)) {
-                        String format = ChatColor.AQUA.toString() + ChatColor.BOLD + "[K] " + ChatColor.YELLOW + player.getDisplayName() + ChatColor.AQUA + ": " + ChatColor.LIGHT_PURPLE + message;
-                        p.sendMessage(format);
-                    }
-                    //Player has SrMod Rank
-                    if (rank.equals(ChatColor.GOLD.toString() + ChatColor.BOLD + Rank.SRMOD)) {
-                        String format = ChatColor.AQUA.toString() + ChatColor.BOLD + "[K] " + ChatColor.GOLD + player.getDisplayName() + ChatColor.AQUA + ": " + ChatColor.LIGHT_PURPLE + message;
-                        p.sendMessage(format);
-                    }
-                    //Player has JrAdmin Rank
-                    if (rank.equals(ChatColor.DARK_RED.toString() + ChatColor.BOLD + Rank.JRADMIN)) {
-                        String format = ChatColor.AQUA.toString() + ChatColor.BOLD + "[K] " + ChatColor.DARK_RED + player.getDisplayName() + ChatColor.AQUA + ": " + ChatColor.LIGHT_PURPLE + message;
-                        p.sendMessage(format);
-                    }
-                    //Player has Admin Rank
-                    if (rank.equals(ChatColor.DARK_RED.toString() + ChatColor.BOLD + Rank.ADMIN)) {
-                        String format = ChatColor.AQUA.toString() + ChatColor.BOLD + "[K] " + ChatColor.DARK_RED + player.getDisplayName() + ChatColor.AQUA + ": " + ChatColor.LIGHT_PURPLE + message;
-                        p.sendMessage(format);
+                        if (plugin.getOwner().containsKey(player.getUniqueId().toString())) {
+                            String format = ChatColor.GOLD.toString() + ChatColor.BOLD + "[K] " + ChatColor.LIGHT_PURPLE + "King " + ChatColor.GOLD + player.getName() + ": " + message;
+                            p.sendMessage(format);
+                        } else if (plugin.getAdmin().containsKey(player.getUniqueId().toString())) {
+                            String format = ChatColor.GOLD.toString() + ChatColor.BOLD + "[K] " + ChatColor.LIGHT_PURPLE + "Knight " + ChatColor.GOLD + player.getName() + ": " + message;
+                            p.sendMessage(format);
+                        } else if (plugin.getMember().containsKey(player.getUniqueId().toString()) && !plugin.getOwner().containsKey(player.getUniqueId().toString())) {
+                            String format = ChatColor.GOLD.toString() + ChatColor.BOLD + "[K] " + ChatColor.LIGHT_PURPLE + "Citizen " + ChatColor.GOLD + player.getName() + ": " + message;
+                            p.sendMessage(format);
+                        }
                     }
                 }
             }
