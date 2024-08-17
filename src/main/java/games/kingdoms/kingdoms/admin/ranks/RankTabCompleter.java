1ffe0c4cc08a9b18d6e1f40e089061a73bc0bbd6
JylerG <pframm4@gmail.com>
2024-08-17 13:37:38 -0500

    Made compatible with console

--- src/main/java/games/kingdoms/kingdoms/admin/ranks/RankTabCompleter.java
+++ src/main/java/games/kingdoms/kingdoms/admin/ranks/RankTabCompleter.java
@@ -14,19 +14,18 @@ public class RankTabCompleter implements TabCompleter {
     @Override
     public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, String[] args) {
 
-        if (sender instanceof Player player) {
-            if (player.hasPermission("kingdoms.setrank.default")
-                    || player.hasPermission("kingdoms.setrank.vip")
-                    || player.hasPermission("kingdoms.setrank.hero")
-                    || player.hasPermission("kingdoms.setrank.jrmod")
-                    || player.hasPermission("kingdoms.setrank.mod")
-                    || player.hasPermission("kingdoms.setrank.srmod")
-                    || player.hasPermission("kingdoms.setrank.jradmin")
-                    || player.hasPermission("kingdoms.setrank.admin")) {
+            if (sender.hasPermission("kingdoms.setrank.default")
+                    || sender.hasPermission("kingdoms.setrank.vip")
+                    || sender.hasPermission("kingdoms.setrank.hero")
+                    || sender.hasPermission("kingdoms.setrank.jrmod")
+                    || sender.hasPermission("kingdoms.setrank.mod")
+                    || sender.hasPermission("kingdoms.setrank.srmod")
+                    || sender.hasPermission("kingdoms.setrank.jradmin")
+                    || sender.hasPermission("kingdoms.setrank.admin")) {
 
                 if (args.length == 1) {
 
-                    if (player.hasPermission("kingdoms.setrank.fake")) {
+                    if (sender.hasPermission("kingdoms.setrank.fake") && sender instanceof Player) {
                         List<String> fake = new ArrayList<>();
                         fake.add("fake");
                         fake.add("set");
@@ -40,7 +39,7 @@ public class RankTabCompleter implements TabCompleter {
                 }
 
                 if (args.length == 2) {
-                    if (player.hasPermission("kingdoms.setrank.fake")) {
+                    if (sender.hasPermission("kingdoms.setrank.fake") && sender instanceof Player) {
                         if (args[0].equalsIgnoreCase("fake")) {
                             List<String> rank = new ArrayList<>();
                             rank.add("<rank>");
@@ -51,12 +50,23 @@ public class RankTabCompleter implements TabCompleter {
 
                 if (args.length == 3) {
                     List<String> ranks = new ArrayList<>();
-                    ranks.add("<rank>");
+                    if (sender instanceof Player player) {
+                        ranks.add("<rank>");
+                    } else {
+                        ranks.add("default");
+                        ranks.add("vip");
+                        ranks.add("hero");
+                        ranks.add("youtube");
+                        ranks.add("jrmod");
+                        ranks.add("mod");
+                        ranks.add("srmod");
+                        ranks.add("jradmin");
+                        ranks.add("admin");
+                    }
 
                     return ranks;
                 }
             }
-        }
         return null;
     }
 }
