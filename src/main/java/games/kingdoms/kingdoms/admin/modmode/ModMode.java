c6397d59c13b138358dba0cab698e217d2087d81
JylerG <pframm4@gmail.com>
2025-03-22 22:09:25 -0500

    Update Code

--- src/main/java/games/kingdoms/kingdoms/admin/modmode/ModMode.java
+++ src/main/java/games/kingdoms/kingdoms/admin/modmode/ModMode.java
@@ -11,9 +11,14 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ModMode implements CommandExecutor {
 
     final Kingdoms plugin = Kingdoms.getPlugin(Kingdoms.class);
+    final HashMap<String, String> staff = plugin.getStaff();
+    final Map<String, String> playerRank = plugin.getPlayerRank();
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
@@ -24,24 +29,24 @@ public class ModMode implements CommandExecutor {
                     plugin.getModModePlayers().add(player);
                     player.setGameMode(GameMode.SPECTATOR);
                     for (Player p : Bukkit.getOnlinePlayers()) {
-                        if (plugin.getStaff().get(p.getUniqueId().toString()).equalsIgnoreCase("JRMOD")
-                                || plugin.getStaff().get(p.getUniqueId().toString()).equalsIgnoreCase("MOD")
-                                || plugin.getStaff().get(p.getUniqueId().toString()).equalsIgnoreCase("SRMOD")
-                                || plugin.getStaff().get(p.getUniqueId().toString()).equalsIgnoreCase("JRADMIN")
-                                || plugin.getStaff().get(p.getUniqueId().toString()).equalsIgnoreCase("ADMIN")) {
-                            if (plugin.getPlayerRank().get(player.getUniqueId().toString()).equalsIgnoreCase(ChatColor.DARK_AQUA.toString() + ChatColor.BOLD + Rank.JRMOD)) {
+                        if (staff.get(p.getUniqueId().toString()).equalsIgnoreCase("JRMOD")
+                                || staff.get(p.getUniqueId().toString()).equalsIgnoreCase("MOD")
+                                || staff.get(p.getUniqueId().toString()).equalsIgnoreCase("SRMOD")
+                                || staff.get(p.getUniqueId().toString()).equalsIgnoreCase("JRADMIN")
+                                || staff.get(p.getUniqueId().toString()).equalsIgnoreCase("ADMIN")) {
+                            if (playerRank.get(player.getUniqueId().toString()).equalsIgnoreCase(ChatColor.DARK_AQUA.toString() + ChatColor.BOLD + Rank.JRMOD)) {
                                 p.sendMessage(ChatColor.DARK_AQUA.toString() + ChatColor.BOLD + Rank.JRMOD + " " + ChatColor.WHITE + player.getName() + ChatColor.GRAY + " has entered modmode. They can now " +
                                         "interact with other players in spectator mode");
-                            } else if (plugin.getPlayerRank().get(player.getUniqueId().toString()).equalsIgnoreCase(ChatColor.YELLOW.toString() + ChatColor.BOLD + Rank.MOD)) {
+                            } else if (playerRank.get(player.getUniqueId().toString()).equalsIgnoreCase(ChatColor.YELLOW.toString() + ChatColor.BOLD + Rank.MOD)) {
                                 p.sendMessage(ChatColor.YELLOW.toString() + ChatColor.BOLD + Rank.MOD + " " + ChatColor.WHITE + player.getName() + ChatColor.GRAY + " has entered modmode. They can now " +
                                         "interact with other players in spectator mode");
-                            } else if (plugin.getPlayerRank().get(player.getUniqueId().toString()).equalsIgnoreCase(ChatColor.GOLD.toString() + ChatColor.BOLD + Rank.SRMOD)) {
+                            } else if (playerRank.get(player.getUniqueId().toString()).equalsIgnoreCase(ChatColor.GOLD.toString() + ChatColor.BOLD + Rank.SRMOD)) {
                                 p.sendMessage(ChatColor.GOLD.toString() + ChatColor.BOLD + Rank.SRMOD + " " + ChatColor.WHITE + player.getName() + ChatColor.GRAY + " has entered modmode. They can now " +
                                         "interact with other players in spectator mode");
-                            } else if (plugin.getPlayerRank().get(player.getUniqueId().toString()).equalsIgnoreCase(ChatColor.DARK_RED.toString() + ChatColor.BOLD + Rank.JRADMIN)) {
+                            } else if (playerRank.get(player.getUniqueId().toString()).equalsIgnoreCase(ChatColor.DARK_RED.toString() + ChatColor.BOLD + Rank.JRADMIN)) {
                                 p.sendMessage(ChatColor.DARK_RED.toString() + ChatColor.BOLD + Rank.JRADMIN + " " + ChatColor.WHITE + player.getName() + ChatColor.GRAY + " has entered modmode. They can now " +
                                         "interact with other players in spectator mode");
-                            } else if (plugin.getPlayerRank().get(player.getUniqueId().toString()).equalsIgnoreCase(ChatColor.DARK_RED.toString() + ChatColor.BOLD + Rank.ADMIN)) {
+                            } else if (playerRank.get(player.getUniqueId().toString()).equalsIgnoreCase(ChatColor.DARK_RED.toString() + ChatColor.BOLD + Rank.ADMIN)) {
                                 p.sendMessage(ChatColor.DARK_RED.toString() + ChatColor.BOLD + Rank.ADMIN + " " + ChatColor.WHITE + player.getName() + ChatColor.GRAY + " has entered modmode. They can now " +
                                         "interact with other players in spectator mode");
                             }
@@ -51,24 +56,24 @@ public class ModMode implements CommandExecutor {
                     plugin.getModModePlayers().remove(player);
                     player.setGameMode(GameMode.SURVIVAL);
                     for (Player p : Bukkit.getOnlinePlayers()) {
-                        if (plugin.getStaff().get(p.getUniqueId().toString()).equalsIgnoreCase("JRMOD")
-                                || plugin.getStaff().get(p.getUniqueId().toString()).equalsIgnoreCase("MOD")
-                                || plugin.getStaff().get(p.getUniqueId().toString()).equalsIgnoreCase("SRMOD")
-                                || plugin.getStaff().get(p.getUniqueId().toString()).equalsIgnoreCase("JRADMIN")
-                                || plugin.getStaff().get(p.getUniqueId().toString()).equalsIgnoreCase("ADMIN")) {
-                            if (plugin.getPlayerRank().get(player.getUniqueId().toString()).equalsIgnoreCase(ChatColor.DARK_AQUA.toString() + ChatColor.BOLD + Rank.JRMOD)) {
+                        if (staff.get(p.getUniqueId().toString()).equalsIgnoreCase("JRMOD")
+                                || staff.get(p.getUniqueId().toString()).equalsIgnoreCase("MOD")
+                                || staff.get(p.getUniqueId().toString()).equalsIgnoreCase("SRMOD")
+                                || staff.get(p.getUniqueId().toString()).equalsIgnoreCase("JRADMIN")
+                                || staff.get(p.getUniqueId().toString()).equalsIgnoreCase("ADMIN")) {
+                            if (playerRank.get(player.getUniqueId().toString()).equalsIgnoreCase(ChatColor.DARK_AQUA.toString() + ChatColor.BOLD + Rank.JRMOD)) {
                                 p.sendMessage(ChatColor.DARK_AQUA.toString() + ChatColor.BOLD + Rank.JRMOD + " " + ChatColor.WHITE + player.getName() + ChatColor.GRAY + " has left modmode. They can no longer " +
                                         "interact with other players in spectator mode");
-                            } else if (plugin.getPlayerRank().get(player.getUniqueId().toString()).equalsIgnoreCase(ChatColor.YELLOW.toString() + ChatColor.BOLD + Rank.MOD)) {
+                            } else if (playerRank.get(player.getUniqueId().toString()).equalsIgnoreCase(ChatColor.YELLOW.toString() + ChatColor.BOLD + Rank.MOD)) {
                                 p.sendMessage(ChatColor.YELLOW.toString() + ChatColor.BOLD + Rank.MOD + " " + ChatColor.WHITE + player.getName() + ChatColor.GRAY + " has left modmode. They can no longer " +
                                         "interact with other players in spectator mode");
-                            } else if (plugin.getPlayerRank().get(player.getUniqueId().toString()).equalsIgnoreCase(ChatColor.GOLD.toString() + ChatColor.BOLD + Rank.SRMOD)) {
+                            } else if (playerRank.get(player.getUniqueId().toString()).equalsIgnoreCase(ChatColor.GOLD.toString() + ChatColor.BOLD + Rank.SRMOD)) {
                                 p.sendMessage(ChatColor.GOLD.toString() + ChatColor.BOLD + Rank.SRMOD + " " + ChatColor.WHITE + player.getName() + ChatColor.GRAY + " has left modmode. They can no longer " +
                                         "interact with other players in spectator mode");
-                            } else if (plugin.getPlayerRank().get(player.getUniqueId().toString()).equalsIgnoreCase(ChatColor.DARK_RED.toString() + ChatColor.BOLD + Rank.JRADMIN)) {
+                            } else if (playerRank.get(player.getUniqueId().toString()).equalsIgnoreCase(ChatColor.DARK_RED.toString() + ChatColor.BOLD + Rank.JRADMIN)) {
                                 p.sendMessage(ChatColor.DARK_RED.toString() + ChatColor.BOLD + Rank.JRADMIN + " " + ChatColor.WHITE + player.getName() + ChatColor.GRAY + " has left modmode. They can no longer " +
                                         "interact with other players in spectator mode");
-                            } else if (plugin.getPlayerRank().get(player.getUniqueId().toString()).equalsIgnoreCase(ChatColor.DARK_RED.toString() + ChatColor.BOLD + Rank.ADMIN)) {
+                            } else if (playerRank.get(player.getUniqueId().toString()).equalsIgnoreCase(ChatColor.DARK_RED.toString() + ChatColor.BOLD + Rank.ADMIN)) {
                                 p.sendMessage(ChatColor.DARK_RED.toString() + ChatColor.BOLD + Rank.ADMIN + " " + ChatColor.WHITE + player.getName() + ChatColor.GRAY + " has left modmode. They can no longer " +
                                         "interact with other players in spectator mode");
                             }
