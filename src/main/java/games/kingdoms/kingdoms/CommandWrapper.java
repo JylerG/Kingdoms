02f3cc7a46825ea0e709e24d3454a6238aec6de8
JylerG <pframm4@gmail.com>
2025-03-24 21:10:43 -0500

    Add CommandWrapper.java

---
+++ src/main/java/games/kingdoms/kingdoms/CommandWrapper.java
@@ -0,0 +1,23 @@
+package games.kingdoms.kingdoms;
+
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class CommandWrapper extends org.bukkit.command.Command {
+
+    private final CommandExecutor executor;
+
+    protected CommandWrapper(String name, CommandExecutor executor) {
+        super(name);
+        this.executor = executor;
+    }
+
+    @Override
+    public boolean execute(@NotNull CommandSender sender, @NotNull String label, String[] args) {
+        if (executor != null) {
+            return executor.onCommand(sender, this, label, args);
+        }
+        return true;
+    }
+}
